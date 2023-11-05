33ca2231e4b8c6b070ba55338b7109e82bda6882
Mamun Ar Rashid <mamuncseruet03@gmail.com>
2023-11-05 23:05:02 +0600

    MamunGPT
    MamunGPT

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,12 @@ import { BoltIcon, ExclamationTriangleIcon, SunIcon } from '@heroicons/react/24/
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center h-screen px-2 text-white">
-      <h1 className="text-5xl font-bold mb-20">ChristianGPT</h1>
+      <h1 className="text-5xl font-bold mb-20">MamunGPT</h1>
       <div className='flex space-x-2 text-center'>
         <div>
           <div className="flex flex-col items-center justify-center mb-5">
             <SunIcon className='h-8 w-8'/>
-            <h2>Let Christian Google that for you!</h2>
+            <h2>Let Google that for you!</h2>
           </div>
           <div className="space-y-2">
             <p className="infoText">Explain something to me</p>
