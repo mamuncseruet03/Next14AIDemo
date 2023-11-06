bf705cd6808866ac53a1b1233bc7018faacdc83b
Mamun Ar Rashid <mamuncseruet03@gmail.com>
2023-11-06 09:17:54 +0600

    Fixed error
    Fixed error

--- src/firebase/firebaseAdmin.ts
+++ src/firebase/firebaseAdmin.ts
@@ -1,8 +1,6 @@
 import admin from "firebase-admin";
 import { getApps } from "firebase-admin/app";
 
-// const serviceAccount = require("./serviceAccountKey.json")
-
 const serviceAccount = JSON.parse(
   process.env.FIREBASE_SERVICE_ACCOUNT_KEY as string
 );
